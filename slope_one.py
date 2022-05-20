fb8efd4041ded90a3ea7660cbeff076791e65f2e
Ben <benmali94@gmail.com>
2022-05-20 20:54:52 +0300

    fixed slope

--- slope_one.py
+++ slope_one.py
@@ -6,6 +6,9 @@ from scipy.sparse import csc_matrix
 import pickle
 from os import path
 from tqdm import tqdm
+import warnings
+
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
 class SlopeOne(Regressor):
@@ -16,12 +19,13 @@ class SlopeOne(Regressor):
             self.upload_params()
 
     def fit(self, X: np.array):
+        self.matrix = csc_matrix((X[:, RATINGS_COL_INDEX], (X[:, USERS_COL_INDEX],X[:, ITEMS_COL_INDEX]))).toarray()
         if not self.popularity_differences:
             self.build_popularity_difference_dict(X)
             self.save_params()
 
     def build_popularity_difference_dict(self, data):
-        self.matrix = csc_matrix((data[:, RATINGS_COL_INDEX], (data[:, USERS_COL_INDEX], data[:, ITEMS_COL_INDEX]))).toarray()
+        #self.matrix = csc_matrix((data[:, RATINGS_COL_INDEX], (data[:, USERS_COL_INDEX], data[:, ITEMS_COL_INDEX]))).toarray()
         #self.matrix = np.array([[5,3,2],[3,4,0],[0,2,5]])
         for i in tqdm(range(self.matrix.shape[1])):
             for j in range(self.matrix.shape[1]):
@@ -31,18 +35,25 @@ class SlopeOne(Regressor):
                         item2 = np.array(self.matrix[:,j], dtype=np.int16)
                         values_vector = np.array(item1 - item2, dtype=np.int16)
                         bool_vector = np.array(item1 * item2) > 0
-                        pd_vec = np.sum(values_vector * bool_vector, dtype=np.float32) / np.sum(bool_vector, dtype=np.float16)
+                        bool_vec_sum = np.sum(bool_vector)
+                        if bool_vec_sum != 0:
+                            pd_vec = np.sum(values_vector * bool_vector, dtype=np.float32) / bool_vec_sum
+                        else:
+                            pd_vec = 0  # Not a single user that ranked both items # TODO - check this
                         self.popularity_differences[(i,j)] = (pd_vec, np.sum(bool_vector, dtype=np.float16))
                         self.popularity_differences[(j,i)] = (- pd_vec, np.sum(bool_vector, dtype=np.float16))
 
     def predict_on_pair(self, user: int, item: int):
-        user_items = self.matrix[user, :]
+        user_items = self.matrix[int(user)]
+        if np.isnan(item):
+            return 3 # TODO - check this
+        item = int(item)
         calc = 0
         total_weight = 0
         for i, rank in enumerate(user_items):
             if rank != 0:
-                calc += (user_items[i] + self.popularity_differences[(item,i)][0]) * self.popularity_differences[(item,i)][1]
-                total_weight += self.popularity_differences[(item,i)][1]
+                calc += (user_items[i] + self.popularity_differences[(item, i)][0]) * self.popularity_differences[(item,i)][1]
+                total_weight += self.popularity_differences[(item, i)][1]
         return calc/total_weight
 
     def upload_params(self):

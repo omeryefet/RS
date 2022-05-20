429c965b4ec2f66bf8a2999e1c11e93f51f0c803
Ben <benmali94@gmail.com>
2022-05-20 10:39:44 +0300

    ben

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 from config import *
 import numpy as np
 
+
 def get_data():
     """
     reads train, validation to python indices so we don't need to deal with it in each algorithm.
@@ -10,20 +11,16 @@ def get_data():
      other indicator that tells us that.
     """
     train = pd.read_csv(TRAIN_PATH)
-    print(train.head())
-    # validation = pd.read_csv(VALIDATION_PATH)
-    c=train[USER_COL_NAME_IN_DATAEST].apply(lambda x: x-1)
-    print(c.head())
-    # validation[USER_COL_NAME_IN_DATAEST].apply(lambda x:x-1)
-
-    
-    # train[USER_COL_NAME_IN_DATAEST] = train[USER_COL_NAME_IN_DATAEST] - 1  #changing the user ids from 0 to n-1 by decreasing all by 1 - Train
-    # validation[USER_COL_NAME_IN_DATAEST] = validation[USER_COL_NAME_IN_DATAEST] - 1  #changing the user ids from 0 to n-1 by decreasing all by 1 - Validation
-    # uniqe_lst = list(train[ITEM_COL_NAME_IN_DATASET].unique()) #list of unique items from train data
-    # new_ids = validation[validation[ITEM_COL_NAME_IN_DATASET].isin(uniqe_lst) == False]  #holding the rows in validation data that have an item that doesnt exists in train data
-    # index_to_change = new_ids.index  #holding the index of these rows
-    # validation[ITEM_COL_NAME_IN_DATASET][index_to_change] = np.nan   
-    # return train, validation
+    validation = pd.read_csv(VALIDATION_PATH)
+    #train[USER_COL_NAME_IN_DATAEST] = train[USER_COL_NAME_IN_DATAEST].apply(lambda x: x-1)
+    #validation[USER_COL_NAME_IN_DATAEST].apply(lambda x:x-1)
+    train[USER_COL_NAME_IN_DATAEST] = train[USER_COL_NAME_IN_DATAEST] - 1  #changing the user ids from 0 to n-1 by decreasing all by 1 - Train
+    validation[USER_COL_NAME_IN_DATAEST] = validation[USER_COL_NAME_IN_DATAEST] - 1  #changing the user ids from 0 to n-1 by decreasing all by 1 - Validation
+    uniqe_lst = list(train[ITEM_COL_NAME_IN_DATASET].unique()) #list of unique items from train data
+    new_ids = validation[validation[ITEM_COL_NAME_IN_DATASET].isin(uniqe_lst) == False]  #holding the rows in validation data that have an item that doesnt exists in train data
+    index_to_change = new_ids.index  #holding the index of these rows
+    validation[ITEM_COL_NAME_IN_DATASET][index_to_change] = np.nan
+    return train, validation
 
 
 class Config:

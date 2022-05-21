4e690d7cb591295db4f23b2172732296780098b7
Omer Yefet <omeryefet7@gmail.com>
2022-05-21 12:04:19 +0300

    item knn

--- item_knn.py
+++ item_knn.py
@@ -3,22 +3,61 @@ import numpy as np
 from interface import Regressor
 from utils import get_data, Config
 
+import pandas as pd
+from scipy.sparse import csr_matrix
+from scipy.sparse import csc_matrix
+from config import *
+from os import path
+import csv
+from scipy.sparse import coo_matrix
+from tqdm import tqdm
+import math
+
 
 class KnnItemSimilarity(Regressor):
     def __init__(self, config):
-        raise NotImplementedError
+        # self.corr_matrix = None  # The similarity matrix for all the pairs of items
+        self.k = config.k
+        self.n_users = None
+        self.n_items = None
+        # self.item_avg_rank = None  # The average rating for each item
+        # self.sim_matrix = None  # Similarity matrix but with sparse matrix
+        # self.rank_matrix = None  # Matrix with users in rows, items in columns and rating as the value
+        # self.rank_matrix_spars = None  # The same matrix as before but with sparse matrix
+        # self.user_avg_rank = None  # # The average rating for each user
 
     def fit(self, X: np.array):
-        raise NotImplementedError
+        self.n_users = X[USER_COL].nunique()
+        self.n_items = X[ITEM_COL].nunique()
+        self.corr_matrix = np.zeros((self.n_items, self.n_items))
+        self.user_avg_rank = X.groupby(USER_COL)[RATING_COL].mean()                     # The average rating for each user
+        self.item_avg_rank = X.groupby(ITEM_COL)[RATING_COL].mean()                     # The average rating for each item
+        self.rank_matrix = train.pivot(index=USER_COL, columns=ITEM_COL, values=RATING_COL)  # Create matrix with users in rows, items in columns and rating as the value
+        self.rank_matrix_spars = csr_matrix(self.rank_matrix)                          # Save the matrix in sparse matrix
+        if path.exists(CORRELATION_PARAMS_FILE_PATH):
+            self.upload_params()
+            spars_df = self.sim_matrix.toarray()  # Transform the similarity sparse matrix to numpy_array
+            for i in range(spars_df.shape[0]):
+                item1 = spars_df[i, 0]
+                item2 = spars_df[i, 1]
+                self.corr_matrix[int(item1), int(item2)] = spars_df[i, 2]  # Save the similarity in corr_matrix from the csv file- include items with no similarity
+            self.corr_matrix = self.corr_matrix + self.corr_matrix.T  # Similarity for (x,y) is the same as (y,x)
+            self.corr_matrix = np.nan_to_num(self.corr_matrix)  # Change nan to 0
+        else:
+            self.build_item_to_itm_corr_dict()
+            self.save_params()
 
     def build_user_to_user_corr_dict(self, data):
         raise NotImplementedError
 
+
     def predict_on_pair(self, user, item):
         raise NotImplementedError
 
+
     def upload_params(self):
         raise NotImplementedError
+
 
     def save_params(self):
         raise NotImplementedError
--- user_knn.py
+++ user_knn.py
@@ -3,27 +3,91 @@ import numpy as np
 from interface import Regressor
 from utils import get_data, Config
 
+from config import *
+from scipy.sparse import csc_matrix
+from tqdm import tqdm
+from collections import defaultdict
+from statistics import mode
+import pickle
+from os import path
+
 class KnnUserSimilarity(Regressor):
     def __init__(self, config):
-        raise NotImplementedError
+        self.matrix = None
+        self.corr = defaultdict(list)
+        self.k = config.k
+        if path.exists(CORRELATION_PARAMS_FILE_PATH):
+            self.upload_params()
 
     def fit(self, X: np.array):
-        raise NotImplementedError
+        X = X.to_numpy(dtype=int)
+        self.matrix = csc_matrix((X[:, RATINGS_COL_INDEX], (X[:, USERS_COL_INDEX],X[:, ITEMS_COL_INDEX]))).toarray()
+        if not self.corr:
+            self.build_item_to_itm_corr_dict(X)
+            self.save_params()
+
 
     def build_item_to_itm_corr_dict(self, data):
-        raise NotImplementedError
+        pairs = set()
+        for i in tqdm(range(self.matrix.shape[0])):
+            for j in range(i+1, self.matrix.shape[0]):
+                pairs.add((i,j))
+                if i != j:
+                    user1 = np.array(self.matrix[i, :], dtype=np.int16)
+                    user2 = np.array(self.matrix[j, :], dtype=np.int16)
+                    bool_vector = np.array(user1 * user2) > 0
+                    user1 = user1[bool_vector]
+                    user2 = user2[bool_vector]
+                    if any(user1 * user2):
+                        # users_corr = np.corrcoef(user1,user2)[0,1]
+                        users_corr = 1
+                    else:
+                        users_corr = 0
+                    self.corr[i].append((j,users_corr))
+                    # self.corr[j].append((i,users_corr))
+        for user,corr_list in self.corr.items():
+            print(len(self.corr[user]))
+            self.corr[user] = sorted(corr_list, key = lambda x: x[1], reverse=True)
 
     def predict_on_pair(self, user: int, item: int):
-        raise NotImplementedError
+        if item != -1:
+            if item == 409:
+                print(item)
+            k_nearest_users = [self.corr[user][i][0] for i in range(self.k)]
+            return mode(k_nearest_users)
+        else:
+            return 3 # TODO - check this
 
     def upload_params(self):
-        raise NotImplementedError
+        with open(CORRELATION_PARAMS_FILE_PATH, 'rb') as file:
+            self.corr = pickle.load(file)
 
+    def save_params(self):
+        with open(CORRELATION_PARAMS_FILE_PATH, 'wb') as file:
+            pickle.dump(self.corr, file, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
     knn_config = Config(k=10)
     train, validation = get_data()
-    train = train[:200000, :]
+    train = train.iloc[:200000, :]
     knn = KnnUserSimilarity(knn_config)
     knn.fit(train)
     print(knn.calculate_rmse(validation))
+
+# import numpy as np
+# a = np.array([0,0,0,0])
+# c = np.array([3,1,2,3])
+# b=a*c>0
+# users_corr = np.corrcoef(c,a)
+# # b=np.array([True,True,False,True])
+# print(a[b])
+# print(c[b])
+# print(users_corr[0,1])
+# import pandas as pd
+# df1=pd.DataFrame(a[b])
+# df2=pd.DataFrame(c[b])
+# print('-----')
+# # print(df1)
+# # print(df2)
+# f=df1.corrwith(df2,method='pearson')
+# print(f)
